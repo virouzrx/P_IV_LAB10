50e3fb9266939cd0acfef6f2948648ab333d642a
Szymon Szałata <szymon.szalata.1@gmail.com>
2020-05-18 16:59:18 +0200

    lab10

---
+++ WpfApp1/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class User
+    {
+        public User(int id, string login, string password, int points)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+            Points = points;
+        }
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        private int _points;
+
+        public int Points
+        {
+            get { return _points; }
+            set
+            {
+                if (_points != value)
+                {
+                    _points = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Points")); //invoke? 
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
