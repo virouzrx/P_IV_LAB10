f3293d19483d45a7f4c16203f4b93d222b0e68e4
Szymon Szałata <szymon.szalata.1@gmail.com>
2020-05-18 16:19:41 +0200

    Lab 10

---
+++ WpfApp1/FileContent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class FileContent : INotifyPropertyChanged
+    {
+   
+
+        private string _content;
+        internal static FileContent DataContext;
+
+        public string Path { get; set; }
+
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                if (_content != value)
+                {
+                    _content = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Content"));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
